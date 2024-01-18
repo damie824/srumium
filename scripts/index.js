6e046c030883933b530cfe47f68a681d16b34631
damie824 <booboo4055@gmail.com>
2024-01-19 01:24:46 +0900

    initialize commit

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+// function dsbs() {
+//   const urls = document.querySelectorAll("a");
+//   const p = document.querySelectorAll("p");
+//   const span = document.querySelectorAll("span");
+//   const h1 = document.querySelectorAll("h1");
+//   const h2 = document.querySelectorAll("h2");
+//   const h3 = document.querySelectorAll("h3");
+//   const h4 = document.querySelectorAll("h4");
+//   const h5 = document.querySelectorAll("h6");
+//   const h6 = document.querySelectorAll("h6");
+
+//   const txts = [
+//     ...urls,
+//     ...p,
+//     ...span,
+//     ...h1,
+//     ...h2,
+//     ...h3,
+//     ...h4,
+//     ...h5,
+//     ...h6,
+//   ];
+
+//   txts.forEach((e) => {
+//     e.innerText = "온 세상이 스럼이야";
+//   });
+// }
+
+// setInterval(dsbs, 0.3);
